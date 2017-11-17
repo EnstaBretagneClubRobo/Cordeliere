e1cade37a8960e4075a1c576c6948633bba0ba1e
Supercelll <yacine.benhnini@ensta-bretagne.org>
2017-11-17 17:36:39 +0100

    Suppression des dossiers build et devel

--- ros_workspace/build/simulator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yacine/Documents/cordeliere/Cordeliere/ros_workspace/src/simulator
-# Build directory: /home/yacine/Documents/cordeliere/Cordeliere/ros_workspace/build/simulator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
