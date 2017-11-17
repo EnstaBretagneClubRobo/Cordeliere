450cee52fb02c3811ad153256a99845836c5d95a
Supercelll <lolATlol.com>
2017-11-17 16:42:05 +0100

    Architecture ROS initialisée

---
+++ ros_workspace/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/yacine/Documents/cordeliere/Cordeliere/ros_workspace/src
+# Build directory: /home/yacine/Documents/cordeliere/Cordeliere/ros_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
