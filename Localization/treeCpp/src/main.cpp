ad2e37a4023fe14d422055f68602a01de791ebce
Clement ROLINAT <rolinacl@p351.ensieta.ecole>
2017-12-15 17:26:29 +0100

    first commit c++ implementation

---
+++ Localization/treeCpp/src/main.cpp
@@ -0,0 +1,13 @@
+#include "tree.h"
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> dim;    
+        
+    Tree tree(vector<int> {1}, vector<float> {1});
+    
+    return 0;
+}
---
+++ Localization/treeCpp/src/tree.cpp
@@ -0,0 +1,14 @@
+#include "tree.h"
+
+using namespace std;
+
+Tree::Tree(vector<int> dims, vector<float> data)
+{
+    
+           
+}
+
+void Tree::fillNode(Node node)
+{
+
+}
